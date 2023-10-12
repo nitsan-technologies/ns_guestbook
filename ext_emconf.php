c387123b8e90a778ae02a9f1ac6c64f3923cf374
nitsan-technologies <info@nitsan.in>
2023-10-12 12:55:22 +0530

    [TASK] Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = [
+$EM_CONF['ns_guestbook'] = [
     'title' => '[NITSAN] Guestbook TYPO3 Plugin',
     'description' => 'One of the only TYPO3 extension which provides to integrate all the features of Guestbook like review form with send mail, list of guest. Demo: https://demo.t3planet.com/t3t-extensions/guestbook/ You can download PRO version for more-features & free-support at https://t3planet.com/typo3-guestbook-extension/',
     'category' => 'plugin',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.2.2',
+    'version' => '3.2.3',
     'constraints' => [
         'depends' => [
             'typo3' => '6.0.0-11.5.99',
